6315f255eeb0296a74c95c77bfdfa6562fa42a02
Chris F <93630113+gonna-boop@users.noreply.github.com>
2023-02-24 00:02:29 -0700

    add first "Book" object
    object created for testing

--- scripts.js
+++ scripts.js
@@ -1,12 +1,35 @@
-let myLibrary = [];
+const hobbit = new Book("The Hobbit", "J.R.R. Tolkein", 304);
 
-function Book(title, author, pages, read) {
-  this.bookName = title;
-  this.authorName = author;
-  this.totalPages = pages;
-  this.readBook = read;
+let myLibrary = [hobbit];
+// first step: figure out how to display items within this array
+// as a card on the page
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+//   this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+//   const addBook = (e) => {
+//     e.preventDefault(); // stops form from submitting
+//     let book = {
+//         title: document.getElementById('title').value,
+//         author: document.getElementById('author').value,
+//         pages: document.getElementById('pages').value,
+//     }
+//   }
 }
+
+// at this point here is what needs to be done
+// the submit button on the form needs to take the
+// inputs and create a book object with them
+// that object then needs to be displayed as a card
+// const form = document.querySelector('form');
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const fd = new FormData(form);
+//   const obj = Object.fromEntries(fd);
+//   console.log(obj);
+// });
